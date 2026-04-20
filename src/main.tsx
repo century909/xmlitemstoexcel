1fadfe93e58a330e1f5e6dfe19efb21815f60b6e
Diego centurion <diegofabianhp@gmail.com>
2026-04-20 19:18:11 -0300

    🔒 Move Google Client ID to env var, add .env.example, update .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const googleClientId = "1069715027621-avmm3fnq277tgfjgeesccs93po3u8fki.apps.googleusercontent.com";
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
